d120898d073f3e33b680a432d2753a338e11ef47
Amir Haris Ahmad <amirharis@gmail.com>
2011-03-31 02:10:19 +0800

    delete

--- ruby/shorturl/app/helpers/launchpad_helper.rb
+++
@@ -1,2 +0,0 @@
-module LaunchpadHelper
-end
--- ruby/shorturl/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Shorturl::Application.config.secret_token = '8f2e43074f1d81b8a6e118216b370d0a59614677b548401aa80a8be8c221b39303d02f0be618acdc3bbdf2a0c7cf8989a08b9bba54df7e30dc2bef5553499e28'
